4e6756e0d9c16714d6b076621354b325c216fd07
Pro <ealamawi@smail.ucas.edu.ps>
2018-12-21 19:06:05 -0800

    first target done

--- client.py
+++ client.py
@@ -9,15 +9,15 @@ while 1:
 
     msg1 = clientSocket.recv(1024).decode()
     num1 = input(msg1)
-    clientSocket.sendall(str.encode(num1))
+    clientSocket.sendall(num1.encode())
 
     msg2 = clientSocket.recv(1024).decode()
     op = input(msg2)
-    clientSocket.sendall(str.encode(op))
+    clientSocket.sendall(op.encode())
 
     msg3 = clientSocket.recv(1024).decode()
     num2 = input(msg3)
-    clientSocket.sendall(str.encode(num2))
+    clientSocket.sendall(num2.encode())
 
     result = clientSocket.recv(1024).decode()
     print('Result: ', result)
--- server.py
+++ server.py
@@ -10,23 +10,24 @@ serverSocket.listen(1)
 print('Ready...')
 
 msg1 = 'Enter First Number: '
-msg1 = 'Enter Operation ( + or - or * or / ): '
-msg1 = 'Enter Second Number: '
+msg2 = 'Enter Operation ( + or - or * or / ): '
+msg3 = 'Enter Second Number: '
 
 while 1:
     connectionSocket, addr = serverSocket.accept()
 
-    connectionSocket.send(str.encode(msg1))
-    num1 = connectionSocket.recv.(1024).decode()
+    connectionSocket.send(msg1.encode())
+    num1 = connectionSocket.recv(1024).decode()
 
-    connectionSocket.send(str.encode(msg2))
+    connectionSocket.send(msg2.encode())
     op = connectionSocket.recv(1024).decode()
 
-    connectionSocket.send(str.encode(msg3))
+    connectionSocket.send(msg3.encode())
     num2 = connectionSocket.recv(1204).decode()
 
-    result = 0;
-    connectionSocket.send(str.encode(result))
+    result = eval(num1 + op + num2)
+    result = str(result)
+    connectionSocket.send(result.encode())
 
     connectionSocket.close()
 
